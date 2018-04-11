2469320b483636ae3d2022817307f4f95e61335e
JacksonMack <35651308+JacksonMack@users.noreply.github.com>
2018-04-11 11:06:05 -0600

    Add files via upload
    added java joc

--- SinglePlayer.java
+++ SinglePlayer.java
@@ -22,12 +22,18 @@ public class SinglePlayer
 	private boolean playerDoneTurn = false;
 	private boolean gameLoaded = false;
 
+/**
+* Getter for board size
+*/
 	public void setBoardSize()
 	{
 		player.setBoardLength(board-1);
 		computer.setBoardLength(board-1);	
 	}
-
+/**
+* Function for changing board size
+* @param x - Size that the grid will be changed too
+*/
 	public void choosingBoardSize(int x)
 	{
 		player.playerBoard.chooseBoardSize(x);
@@ -35,12 +41,16 @@ public class SinglePlayer
 		computer.playerBoard.chooseBoardSize(x);
 		computer.enemyBoard.chooseBoardSize(x);
 	}
-
-
+/**
+* Gets the previous saved game
+*/
 	public boolean getGameLoaded()
 	{
 		return gameLoaded;
 	}
+/**
+* Saves the current loaded game
+*/
 	public void save()
 	{
 		String fileName = "output.txt";
@@ -73,7 +83,6 @@ public class SinglePlayer
 			outputStream.println();
 		}
 
-
 		for(int i = 0; i < computer.getBoardLength(); i++)
 		{
 			for(int j = 0; j < computer.getBoardLength(); j++)
@@ -94,7 +103,9 @@ public class SinglePlayer
 		outputStream.println(playerturn);
 		outputStream.close();
 	}
-
+/**
+* reads the previous played game from text file
+*/
 	public void load()
 	{
 		String fileName = "output.txt";
@@ -172,7 +183,6 @@ public class SinglePlayer
 			inputStream.nextLine();
 		}
 
-
 		for(int i = 0; i < computer.getBoardLength(); i++)
 		{
 			for(int j = 0; j < computer.getBoardLength(); j++)
@@ -192,7 +202,10 @@ public class SinglePlayer
 		playerturn = inputStream.nextBoolean();
 		gameLoaded = true;
 	}
-
+/**
+* Initializes the single player game, places ships ect.
+* @param gameMode - Ensures that the set game mode is == 1
+*/
 	public void singlePlayerGame(int gameMode)
 	{
 		if(gameMode == 1)
@@ -394,7 +407,9 @@ public class SinglePlayer
 		}
 		System.out.println("TESTING: Board has been linked");
 	}
-
+/**
+* Rotates turns between player and computer 
+*/
 	public void playerTurn()
 	{
 		int playerShipsSunk = 0;
